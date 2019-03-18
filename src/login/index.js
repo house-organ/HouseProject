751eeba13242a0a44ad9561fe9ffc064866c333c
zhixi0912 <382893000@qq.com>
2019-03-18 15:23:53 +0800

    'init'

---
+++ src/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.less'
+
+export default class login extends React.Component{
+    state = {
+
+    }
+    render() {
+        return(
+            <div>login</div>
+        )
+    }
+}
