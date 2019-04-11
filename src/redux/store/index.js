ce26cff302314f4c5dc33a27e1101b4ce1244eec
zhixi0912 <382893000@qq.com>
2019-04-11 18:46:16 +0800

    redux

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,1 +1,8 @@
-import  {} from 'redux'
+// src\redux\store\index.js
+/*
+ *  引入createStore 保存数据源
+ */
+import  {createStore} from 'redux'
+import reducer from './../reducer'
+
+export default ()=>createStore(reducer)
